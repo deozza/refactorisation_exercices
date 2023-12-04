e9e400e8132e28707f9a8a16070e23380dfe7546
deozza <edenn.touitou@letsjam.fr>
2023-12-04 12:59:57 +0100

    dead code exercice corrected

--- src/Controller/DeadCodeController.php
+++ src/Controller/DeadCodeController.php
@@ -12,68 +12,36 @@ class DeadCodeController extends AbstractController
     #[Route('/exercices/deadcode/1', name: 'exercise_deadcode_1', methods: ['POST'])]
     public function getFactoriel(Request $request): Response
     {
-
-        // On récupère les données envoyées par l'utilisateur
         /**
-         * @param array $data
+         * @param array $userInput
          */
-        $data = json_decode($request->getContent(), true);
-        $message = '';
+        $userInput = json_decode($request->getContent(), true);
+        $badRequestResponse = new Response('Invalid data', Response::HTTP_BAD_REQUEST);
 
-        // Si l'utilisateur n'a pas envoyé de données, on retourne une erreur
-        if(isset($data['number'])){
-            $number = $data['number'];
+        if(isset($userInput['number'])){
+            $startingNumber = $userInput['number'];
 
-            // Si la valeur recue n'est pas un nombre, on retourne une erreur
-            if(ctype_digit($number) === false){
-                $message = 'La valeur doit être un nombre';
-                return new Response('Invalid data', 400);
+            if(ctype_digit($startingNumber) === false){
+                return $badRequestResponse;
             }
 
-            // Si la valeur recue est un nombre négatif, on retourne une erreur
-            if($number > 0){
-                // On calcule le factoriel
-                /*
-                // ETO 20/01/2022 : on faisait une boucle for avant, mais on a appris a faire de la recursive depuis 
-                $factoriel = 1;
-                for($i = 1; $i <= $number; $i++){
-                    $factoriel *= $i;
-                }
-                return new Response($factoriel, 200);
-                */
-
-                // ETO 20/01/2022 : on fait de la recursive pour calculer le factoriel
-                /**
-                 * @param int $number
-                 */
-                $factoriel = $this->getFactorielRecursive($number);
-                return new Response($factoriel, 200);
-            }elseif($number === 0){
-                $message = 'Le résultat est 1';
-                return new Response(1, 200);
-            }else{
-                $message = 'Le nombre doit être positif';
-                return new Response('Invalid data', 400);
+            if($startingNumber > 0){
+                $factoriel = $this->getFactorielRecursive($startingNumber);
+                return new Response($factoriel, Response::HTTP_OK);
             }
-        }else{
-            $message = 'Il faut envoyer des données';
-            return new Response('Invalid data', 400);
+            
+            return $badRequestResponse;
         }
-
-        return $this->render('dead_code/index.html.twig', [
-            'controller_name' => 'DeadCodeController',
-            'data' => $data,
-            'message' => $message
-        ]);
+        
+        return $badRequestResponse;
     }
 
-    // ETO 20/01/2022 : on fait de la recursive pour calculer le factoriel
-    private function getFactorielRecursive(int $number): int
+    private function getFactorielRecursive(int $startingNumber): int
     {
-        if($number === 1){
+        if($startingNumber === 1){
             return 1;
-        }else{
-            return $number * $this->getFactorielRecursive($number - 1);
         }
+
+        return $startingNumber * $this->getFactorielRecursive($startingNumber - 1);
     }
 }
