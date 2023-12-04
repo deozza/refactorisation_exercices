59227314341233669a0c2b45be2eee42e08c62e8
deozza <edenn.touitou@letsjam.fr>
2023-12-04 17:45:34 +0100

    fail fast corrected

--- src/Controller/FailFastController.php
+++ src/Controller/FailFastController.php
@@ -15,67 +15,70 @@ class FailFastController extends AbstractController
     #[Route('/exercices/failfast/1', name: 'exercices_failfast_1', methods: ['GET'])]
     public function getProfile(Request $request, UserService $userService): Response
     {
-        $token = $request->headers->get('Authorization');
+        $userToken = $request->headers->get('Authorization');
+        $expectedTokenNeedle = 'superxtrasecrettoken';
+        $unauthorizedResponse = new Response('Unauthorized', Response::HTTP_UNAUTHORIZED);
 
-        if($token !== null){
-            if(strpos($token, 'superxtrasecrettoken') !== false){
-                $user = $userService->getUserByToken($token);
-                if($user !== null){
-                    return new Response('Hello '.$user, 200);
-                }else{
-                    return new Response('Unauthorized', 401);
-                }
-            }else{
-                return new Response('Unauthorized', 401);
-            }
-        }else{
-            return new Response('Unauthorized', 401);
+        if($userToken === null){
+            return $unauthorizedResponse;
         }
+
+        if(strpos($userToken, $expectedTokenNeedle) === false){
+            return $unauthorizedResponse;
+        }
+
+        $user = $userService->getUserByToken($userToken);
+        if($user === null){
+            return $unauthorizedResponse;
+        }
+
+        return new Response('Hello '.$user, Response::HTTP_OK);
     }
 
     #[Route('/exercices/failfast/2/{brand}/{model}', name: 'exercices_failfast_2', methods: ['PATCH'])]
     public function updateCar(Request $request, EntityManagerInterface $em, string $brand, string $model): Response
     {   
-        $token = $request->headers->get('Authorization');
+        $userToken = $request->headers->get('Authorization');
+        $expectedToken = 'iamthecaptainnow';
 
-        if($token !== null){
-            if($token === 'iamthecaptainnow'){
-
-                $car = $em->getRepository(Car::class)->findOneBy([
-                    'brand' => $brand,
-                    'model' => $model
-                ]);
-        
-                if($car !== null){
-                    $data = json_decode($request->getContent(), true);
-                    $form = $this->createForm(UpdateCarType::class, $car);
-        
-                    $form->submit($data);
-        
-                    if($form->isValid()){
-
-                        if($car->getGeabox() === 'manual'){
-                            if($car->getEngine() === 'electric'){
-                                return new Response('Invalid data', 400);
-                            }elseif($car->getEngine() === 'hybrid'){
-                                return new Response('Invalid data', 400);
-                            }
-                        }
-
-                        $em->flush();
-        
-                        return new Response('Car updated', 200);
-                    }else{
-                        return new Response('Invalid data', 400);
-                    }
-                }else{
-                    return new Response('Car not found', 404);
-                }
-            }else{
-                return new Response('Forbidden', 403);
-            }
-        }else{
-            return new Response('Unauthorized', 401);
+        if($userToken === null){
+            return new Response('Unauthorized', Response::HTTP_UNAUTHORIZED);
         }
+
+        if($userToken !== $expectedToken){
+            return new Response('Forbidden', Response::HTTP_FORBIDDEN);
+        }
+
+        $car = $em->getRepository(Car::class)->findOneBy([
+            'brand' => $brand,
+            'model' => $model
+        ]);
+
+        if($car === null) {
+            return new Response('Car not found', Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+        $form = $this->createForm(UpdateCarType::class, $car);
+
+        $form->submit($data);
+
+        if($form->isValid() === false){
+            return new Response('Invalid data', Response::HTTP_BAD_REQUEST);
+        }
+
+        $enginesNotCompatibleWithManual = [
+            'electric',
+            'hybrid'
+        ];
+
+        if($car->getGeabox() === 'manual' && in_array($car->getEngine(), $enginesNotCompatibleWithManual) === false){
+            return new Response('Invalid data', Response::HTTP_BAD_REQUEST);
+        }
+
+        $em->flush();
+
+        return new Response('Car updated', Response::HTTP_OK);
+        
     }
 }
